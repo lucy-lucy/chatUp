16a484c783ebca618d7d45c2407e6ba57fdeec62
lucy-lucy <jackiexmelt@gmail.com>
2019-05-22 22:29:56 +0300

    Updates

---
+++ precache-manifest.4f69c5036be181a220833d04114e4469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a69b05d3a6cd1596326d96447dd61b",
+    "url": "/chatUp/index.html"
+  },
+  {
+    "revision": "147ca599bb0dd697b1be",
+    "url": "/chatUp/static/css/main.ed322a3a.chunk.css"
+  },
+  {
+    "revision": "ed073132462051400627",
+    "url": "/chatUp/static/js/2.36503d98.chunk.js"
+  },
+  {
+    "revision": "147ca599bb0dd697b1be",
+    "url": "/chatUp/static/js/main.d698935b.chunk.js"
+  },
+  {
+    "revision": "05520ea74ae6f4a81aed",
+    "url": "/chatUp/static/js/runtime~main.26c1cb5a.js"
+  },
+  {
+    "revision": "d99a65292d8d10b312fc9279f18eaa41",
+    "url": "/chatUp/static/media/BalooBhai-Regular.d99a6529.ttf"
+  },
+  {
+    "revision": "09de02d05695441e0a38803fd30a3687",
+    "url": "/chatUp/static/media/background.09de02d0.jpg"
+  },
+  {
+    "revision": "005444d4d9fff46ab16236d18fd42d54",
+    "url": "/chatUp/static/media/ottonova_guy.005444d4.png"
+  }
+]);
